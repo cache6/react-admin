2ef720960aa53851d6a5e09cd7e55f42b528b3c4
cache6 <cache6.dev@gmail.com>
2024-06-09 23:26:08 +0900

    react-admin next supabase

---
+++ components/AdminApp.tsx
@@ -0,0 +1,20 @@
+// in src/components/AdminApp.tsx
+"use client"; // remove this line if you choose Pages Router
+import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+const AdminApp = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="posts"
+      list={ListGuesser}
+      edit={EditGuesser}
+      recordRepresentation="title"
+    />
+    <Resource name="comments" list={ListGuesser} edit={EditGuesser} />
+  </Admin>
+);
+
+export default AdminApp;
